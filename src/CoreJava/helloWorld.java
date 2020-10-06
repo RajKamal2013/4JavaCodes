6f14c82321202fb6afb8a6d0372da4ba6419a91f
Raj Kamal <rajkamal2013@gmail.com>
2020-10-06 07:50:20 +0530

    Up n Running

---
+++ src/CoreJava/helloWorld.java
@@ -0,0 +1,8 @@
+package CoreJava;
+
+public class helloWorld {
+    public static void main(String[] var) {
+        System.out.println("Hello World");
+        System.out.println("I am Raj Kamal <Howard Skywalker>");
+    }
+}
