cb3fc741cb9447f823a3ec9270c777a3d1cb9053
Raj Kamal <rajkamal2013@gmail.com>
2020-10-08 09:22:20 +0530

    test

--- src/CoreJava/helloWorld.java
+++ src/CoreJava/helloWorld.java
@@ -2,7 +2,9 @@ package CoreJava;
 
 public class helloWorld {
     public static void main(String[] var) {
+        int num = 45;
         System.out.println("Hello World");
         System.out.println("I am Raj Kamal <Howard Skywalker>");
+        System.out.println("Number is " + num);
     }
 }
